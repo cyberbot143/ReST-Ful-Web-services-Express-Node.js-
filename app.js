01f559b213ede3e1a6cafe933325e04c2ecff626
Cyberbot143 <krishnasai952@gmail.com>
2017-09-22 17:17:45 -0400

    Your message

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+mongoose = require('mongoose'),
+bodyParser = require('body-parser');;
+mongoose.connect("mongodb://localhost:27017/bookAPI"); //connection to mongodb
+var Book = require('./models/bookModel');
 
+var app = express();
 // if port is not set in environment ,the use port 3000
 var port = process.env.PORT || 8000;
 
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-app.use('/api',bookRouter);
-bookRouter.route('/Books').get(function(req,res){
-    var responsejson = {hello:"Hello"};
-    res.json(responsejson);
-})
+bookRouter = require('./Routes/bookRoutes')(Book);
 
+app.use('/api/Books',bookRouter);
 
 app.get('/', function (req, res) {
    res.send('Welcome to my API');
