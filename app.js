412ea7b7665b411f760bdf37dce967e5bf95631f
Cyberbot143 <krishnasai952@gmail.com>
2017-09-24 00:50:06 -0400

    supertest and integrated test

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 var express = require('express'),
-mongoose = require('mongoose'),
-bodyParser = require('body-parser');;
-mongoose.connect("mongodb://localhost:27017/bookAPI"); //connection to mongodb
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+
+var db;
+console.log('Hello');
+if(process.env.ENV == 'Test'){
+
+    db = mongoose.connect('mongodb://localhost/bookAPI_test');
+}
+
+else{
+    db= mongoose.connect('mongodb://localhost/bookAPI');
+}
+
 var Book = require('./models/bookModel');
 
 var app = express();
-// if port is not set in environment ,the use port 3000
-var port = process.env.PORT || 8000;
+
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 bookRouter = require('./Routes/bookRoutes')(Book);
 
-app.use('/api/Books',bookRouter);
 
-app.get('/', function (req, res) {
-   res.send('Welcome to my API');
-})
+app.use('/api/books', bookRouter); 
 
-var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
 
-   console.log("Magic Happens at http://%s:%s", host, port);
+app.get('/', function(req, res){
+    res.send('welcome to my API!');
+});
 
-})
+app.listen(port, function(){
+    console.log('Gulp is running my app on  PORT: ' + port);
+});
+
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
-var gulp = require('gulp');
-nodemon = require('gulp-nodemon');
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    gulpMocha = require('gulp-mocha'),
+    env = require('gulp-env'),
+    supertest = require('supertest');
 
-gulp.task('default',function(){
+
+gulp.task('default', function(){
     nodemon({
-    script:'app.js',
-    ext:'js',
-    env:{
-        PORT:8000
-    },
-    ignore:['./node_modules/**']
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            PORT:8000
+        },
+        ignore: ['./node_modules/**']
     })
-    .on('restart',function(){
-        console.log("***************************************************** ");
-        console.log("************ // Restarting Server// ***************** ");
-        console.log("***************************************************** ");
-    })
-})
+    .on('restart', function(){
+        console.log('Restarting');
+    });
+});
+
+gulp.task('test', function(){
+    env({vars: {ENV:'Test'}});
+    gulp.src('tests/*.js', {read: false})
+        .pipe(gulpMocha({reporter: 'nyan'}))
+});
